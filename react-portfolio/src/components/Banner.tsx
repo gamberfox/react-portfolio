21bae313e4c839137bcc138e9d6d6b5a0c4c5224
gamberfox <gamberfox@gmail.com>
2024-06-19 18:30:20 -0500

    more

---
+++ react-portfolio/src/components/Banner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <>
+      <div>Banner</div>
+      <img src="../assets/react.svg" alt="fail"></img>
+    </>
+  );
+};
+export default Banner;
