a9b7364dd56e578e9429646fd4e8304fc0ad6f13
gamberfox <gamberfox@gmail.com>
2024-06-20 15:14:05 -0500

    more

--- react-portfolio/src/components/Menu.tsx
+++ react-portfolio/src/components/Menu.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Container, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/link.css";
+
 export const Menu = () => {
   const [tab, setTab] = useState("-1");
   const handleSelect = (key: string | null) => {
@@ -12,43 +13,43 @@ export const Menu = () => {
   return (
     <div>
       <Container>
-        <Nav
-          className="justify-content-center py-3"
-          style={{ backgroundColor: "lightblue", fontSize: 30 }}
-          activeKey={tab}
-          onSelect={handleSelect}
-        >
-          <Nav.Item className="mx-7">
+        <Nav className="javi-nav" activeKey={tab} onSelect={handleSelect}>
+          <Nav.Item>
             <Nav.Link
               as={Link}
               eventKey="0"
               to="/"
-              className={tab === "0" ? "javi-link-clicked" : "javi-link"}
+              className={tab === "0" ? "javi-link-clicked active" : "javi-link"}
             >
               Home
             </Nav.Link>
           </Nav.Item>
-          <Nav.Item className="mx-20">
-            <Nav.Link as={Link} eventKey="1" to="/about" className="javi-link">
+          <Nav.Item>
+            <Nav.Link
+              as={Link}
+              eventKey="1"
+              to="/about"
+              className={tab === "1" ? "javi-link-clicked" : "javi-link"}
+            >
               About
             </Nav.Link>
           </Nav.Item>
-          <Nav.Item className="mx-20">
+          <Nav.Item>
             <Nav.Link
               as={Link}
               eventKey="2"
               to="/contact"
-              className="javi-link"
+              className={tab === "2" ? "javi-link-clicked" : "javi-link"}
             >
-              Contact
+              Contact to another world
             </Nav.Link>
           </Nav.Item>
-          <Nav.Item className="mx-2">
+          <Nav.Item>
             <Nav.Link
               as={Link}
               eventKey="3"
               to="/contact"
-              className="javi-link"
+              className={tab === "3" ? "javi-link-clicked active" : "javi-link"}
             >
               square
             </Nav.Link>
