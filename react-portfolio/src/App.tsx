82fad8f28129d6ff4bf2ea9b979a7c1ac8912cd3
gamberfox <gamberfox@gmail.com>
2024-06-19 20:05:00 -0500

    morw

--- react-portfolio/src/App.tsx
+++ react-portfolio/src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Banner from "./components/Banner";
@@ -10,7 +12,7 @@ function App() {
   return (
     <>
       <div>
-        <img src="./assets/react.svg" alt="salero" />
+        <img src="/vite.svg" alt="salero" />
       </div>
 
       <Banner />
--- react-portfolio/src/components/Banner.tsx
+++ react-portfolio/src/components/Banner.tsx
@@ -1,10 +1,61 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap-grid.css";
 
 const Banner = () => {
   return (
     <>
       <div>Banner</div>
-      <img src="../assets/react.svg" alt="fail"></img>
+      <div className="container">
+        <div className="row">
+          <div className="card mb-3" style={{ width: "540px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img
+                  src="javi5.jpg"
+                  className="img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">
+                    Javier Humberto Grijalba Camayo
+                  </h5>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col">
+            <img
+              src="javi5.jpg"
+              alt="fail"
+              style={{ width: "200px", height: "auto" }}
+            />
+          </div>
+          <div className="col">
+            <a href="https://github.com/gamberfox" target="_blank">
+              <img
+                src="github-mark.svg"
+                alt="fail"
+                style={{ width: "200px", height: "auto" }}
+              />
+            </a>
+          </div>
+          <div className="col">asdgasgdasdf</div>
+        </div>
+      </div>
     </>
   );
 };
