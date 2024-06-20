6248db39ccdad5e3e135aecc72f37a2f07dbbc47
gamberfox <gamberfox@gmail.com>
2024-06-20 15:55:44 -0500

    merging issues wow

--- react-portfolio/src/components/Menu.tsx
+++ react-portfolio/src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Container, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
