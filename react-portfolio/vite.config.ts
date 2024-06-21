717f37a0284cb1b62d9fa1a2b96dd2918f9b737e
gamberfox <gamberfox@gmail.com>
2024-06-21 08:16:24 -0500

    rollup

--- react-portfolio/vite.config.ts
+++ react-portfolio/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'vue', // Exclude 'vue' from the bundle
+        'react', // Exclude 'react' from the bundle
+        'some-other-lib', // Exclude another library from the bundle
+        "i18next-http-backend"
+      ]
+    }
+  }
 })
